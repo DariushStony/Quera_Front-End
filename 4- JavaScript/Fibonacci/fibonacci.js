4223fa22c280e41d1c09af57d63c810aeb9bae05
Dariush Hadipour <dariushhadi87@gmail.com>
2021-06-30 12:06:35 +0430

    Update fibonacci with other solution

--- 4- JavaScript/Fibonacci/fibonacci.js
+++ 4- JavaScript/Fibonacci/fibonacci.js
@@ -24,3 +24,22 @@ else {
     }
     console.log(str);
 }
+
+//Second Method
+// let n = parseInt(readline());
+
+// let arr = [];
+
+// for (let i = 0; i < n; i++) {
+//     if (i == 0) {
+//         arr[i] = 0;
+//     }
+//     else if (i == 1) {
+//         arr[i] = 1;
+//     }
+//     else {
+//         arr[i] = arr[i - 2] + arr[i - 1];
+//     }
+// }
+
+// console.log(arr.join(", "));
